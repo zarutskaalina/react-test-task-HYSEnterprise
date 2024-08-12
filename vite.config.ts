6da8e30038583b1992f9cf2168a61a13c4836feb
Zarutska Alina <zaryckaalina@gmail.com>
2024-08-12 16:55:12 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "react-test-task-HYSEnterprise",
+  base: "/react-test-task-HYSEnterprise/",
   build: {
     outDir: "dist",
   },
